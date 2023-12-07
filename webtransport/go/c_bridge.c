8ddd843303e43b1899b28c4de48f94f878a6facd
Paul <pjohnson@minitab.de>
2023-12-07 11:58:02 +0100

    format

--- webtransport/go/c_bridge.c
+++ webtransport/go/c_bridge.c
@@ -1,18 +1,19 @@
 #include "c_bridge.h"
 
-void bridge_new_connection(void* webstream_manager, int session_id, void* web_session_ptr, OnNewConnection f)
+void bridge_new_connection(void *webstream_manager, int session_id, void *web_session_ptr, OnNewConnection f)
 {
    f(webstream_manager, session_id, web_session_ptr);
 }
-void bridge_connection_closed(void* webstream_manager, int session_id, OnConnectionClosed f)
+void bridge_connection_closed(void *webstream_manager, int session_id, OnConnectionClosed f)
 {
    f(webstream_manager, session_id);
 }
-void bridge_client_packet(void* webstream_manager, int session_id, uint16_t opcode, void* struct_ptr, OnClientPacket f)
+void bridge_client_packet(void *webstream_manager, int session_id, uint16_t opcode, void *struct_ptr, OnClientPacket f)
 {
    f(webstream_manager, session_id, opcode, struct_ptr);
 }
 
-void bridge_error(void* webstream_manager, char* bytes, OnError f) {
+void bridge_error(void *webstream_manager, char *bytes, OnError f)
+{
    f(webstream_manager, bytes);
 }
